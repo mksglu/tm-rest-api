206a1be6c8f95b8ea0c35e79f3a7358ab582773b
Mert Koseoglu <info@mkoseoglu.com>
2018-11-10 01:03:24 +0300

    tests

--- src/utils/test.utils.ts
+++ src/utils/test.utils.ts
@@ -1,8 +1,14 @@
-export const TEST_USER_ID = "testuserid";
 export const INVITE_EMAIL = "inviteduser@tdsmaker.com";
-export const mockUser = {
+export const ALREADY_MEMBER_EMAIL = "already-member@tdsmaker.com";
+export const userRouteMockUser: any = {
   firstName: "Mert",
   lastName: "Koseoglu",
   password: "!2E456",
   email: "mert@tdsmaker.com"
+};
+export const userServiceMockUser: any = {
+  firstName: "Duygu",
+  lastName: "Aksoy",
+  password: "!2E456",
+  email: "duygu@tdsmaker.com"
 };

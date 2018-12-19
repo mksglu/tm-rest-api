8d21a862ed906cea41eb7c667e649b3f7df1e585
Mert Koseoglu <info@mkoseoglu.com>
2018-12-19 18:58:03 +0300

    pdf helper

---
+++ src/utils/pdf.helper.ts
@@ -0,0 +1,46 @@
+import * as crypto from "crypto";
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as puppeteer from "puppeteer";
+import config from "../config";
+import { Logs } from "../models";
+
+const createLogger = async error => {
+  try {
+    const Log = new Logs({
+      _id: crypto
+        .createHash("md5")
+        .update(`${new Date().getUTCMilliseconds()}`)
+        .digest("hex"),
+      logMessage: error
+    });
+    await Log.save();
+  } catch (error) {
+    throw Error(error);
+  }
+};
+const pdf = async (accountID: string, datasheetID: string, productName: string, version: string, language: string, html: any): Promise<string> => {
+  try {
+    if (!accountID || datasheetID || productName || version || language || html) {
+      throw "some parameters are undefined or null.";
+    }
+    const rootPath: any = config.pdf.path;
+    const rootDir = path.resolve("./");
+    const fullPath = `${rootDir}/${rootPath}/${accountID}/${datasheetID}/${version}/${language}`;
+    await fs.ensureDirSync(fullPath);
+    const browser = await puppeteer.launch();
+    const tab = await browser.newPage();
+    await tab.setContent(html);
+    await tab.pdf({ path: `${fullPath}/${productName}.pdf`, format: "A4" });
+    await browser.close();
+    return language;
+  } catch (error) {
+    try {
+      createLogger(error);
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+};
+
+export default pdf;
